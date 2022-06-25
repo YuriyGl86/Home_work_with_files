7bcfd2e936ff23e8c60ea351c8f4f232eac08529
YuriyGl86 <yuriygl86@gmail.com>
2022-06-25 11:46:39 +0300

    дескриптор

--- Tests.py
+++ Tests.py
@@ -1,1 +1,55 @@
-test1
+class ValidateString:
+
+    def __init__(self, min_length=3, max_length=100):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def validate(self, string):
+        if type(string) == str and self.min_length <= len(string) <= self.max_length:
+            return True
+        else:
+            return False
+
+
+class StringValue:
+
+    def __init__(self, validator):
+        self.validator = validator
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __set__(self, instance, value):
+        if self.validator.validate:
+            instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+
+class RegisterForm:
+    login = StringValue(ValidateString())
+    password = StringValue(ValidateString())
+    email = StringValue(ValidateString())
+
+    def __init__(self, login, password, email):
+        self.login = login
+        self.password = password
+        self.email = email
+
+    def get_fields(self):
+        return [self.login, self.password, self.email]
+
+    def show():
+        print(f'<form>\nЛогин: {self.login}\nПароль: {self.password}\nEmail: {self.email}\n</form>')
+
+
+
+
+
+
+
+
+
+
+
