061d0d708f9787a8ee419b047419b59368eb86f6
YuriyGl86 <yuriygl86@gmail.com>
2022-07-01 07:29:32 +0300

    дескриптор

--- Tests.py
+++ Tests.py
@@ -1,58 +1,13 @@
-class StackObj:
+class TrackLine:
 
-    def __init__(self, data, next=None):
-        self.__data = data
-        self.__next = next
-
-    @property
-    def data(self):
-        return self.__data
-
-    @data.setter
-    def data(self, new):
-        self.__data = new
-
-    @property
-    def next(self):
-        return self.__next
-
-    @next.setter
-    def next(self, new):
-        self.__next = new
+    def __init__(self, to_x, to_y, max_speed):
+        self.to_x = to_x
+        self.to_y = to_y
+        self.max_speed = max_speed
 
 
-class Stack:
+class Track:
 
-    def __init__(self):
-        self.top = None
-
-    def push_back(self, obj):
-        if self.top is None:
-            self.top = obj
-        else:
-            temp = self.top
-            while temp.next:
-                temp = temp.next
-            temp.next = obj
-
-    def pop_back(self):
-        temp = self.top
-        while temp.next.next:
-            temp = temp.next
-        temp.next = None
-
-    def __add__(self, other):
-        self.push_back(StackObj(other))
-        return self
-
-    def __mul__(self, other):
-        for i in other:
-            self.push_back(StackObj(i))
-        return self
-
-
-
-
-
-
-
+    def __init__(self, start_x, start_y):
+        self.start_x = start_x
+        self.start_y = start_y
