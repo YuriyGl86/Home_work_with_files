b99b56c9ec8150e3a2f56da8593ba9009cb43ab3
YuriyGl86 <yuriygl86@gmail.com>
2022-07-15 07:14:05 +0300

    Final Version 2

--- Home_work_with_files.py
+++ Home_work_with_files.py
@@ -3,20 +3,6 @@ import os
 
 
 #  Task 1
-def get_recepts_from_file(name):
-    with open(name, 'r', encoding='utf-8') as file:
-        lst = file.readlines()
-
-    lst1 = [i.strip() for i in lst]
-    recepts_number = lst1.count('')
-    res = []
-    for i in range(recepts_number):
-        indx = lst1.index('')
-        res.append(lst1[:indx])
-        del lst1[:indx + 1]
-    res.append(lst1)
-    return res
-
 
 def get_dict_for_ingredients(strk):
     ingredient_name, quantity, measure = strk.split(' | ')
@@ -26,11 +12,50 @@ def get_dict_for_ingredients(strk):
 
 def get_cook_book():
     name = input('Ввудите имя файла или путь к файлу: ')
-    dish_list = get_recepts_from_file(name)
-    cooking_book = {}
-    for dish in dish_list:
-        cooking_book[dish[0]] = [get_dict_for_ingredients(ingredient) for ingredient in dish[2:]]
-    return cooking_book
+    with open(name, 'r', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            ingr_list = []
+            name = line.strip()
+            num = int(file.readline().strip())
+            for _ in range(num):
+                ingr = file.readline().strip()
+                ingredient = get_dict_for_ingredients(ingr)
+                ingr_list.append(ingredient)
+            cook_book[name] = ingr_list
+            file.readline()
+    return cook_book
+
+## VERSION 2:
+
+# def get_recepts_from_file(name):
+#     with open(name, 'r', encoding='utf-8') as file:
+#         lst = file.readlines()
+#
+#     lst1 = [i.strip() for i in lst]
+#     recepts_number = lst1.count('')
+#     res = []
+#     for i in range(recepts_number):
+#         indx = lst1.index('')
+#         res.append(lst1[:indx])
+#         del lst1[:indx + 1]
+#     res.append(lst1)
+#     return res
+#
+#
+# def get_dict_for_ingredients(strk):
+#     ingredient_name, quantity, measure = strk.split(' | ')
+#     quantity = int(quantity)
+#     return {'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure}
+#
+#
+# def get_cook_book():
+#     name = input('Ввудите имя файла или путь к файлу: ')
+#     dish_list = get_recepts_from_file(name)
+#     cooking_book = {}
+#     for dish in dish_list:
+#         cooking_book[dish[0]] = [get_dict_for_ingredients(ingredient) for ingredient in dish[2:]]
+#     return cooking_book
 
 
 cook_book = get_cook_book()
